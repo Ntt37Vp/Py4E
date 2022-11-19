d44e2b53bf9fe98bcd4b6bef153cbfe9f8686904
Ntt37Vp <fuckspammers@protonmail.com>
2022-11-19 08:04:23 +0800

    course 3 continuation

--- Course 3 - Using Python to Access Web Data/xml1.py
+++ Course 3 - Using Python to Access Web Data/xml1.py
@@ -1,12 +1,14 @@
 import xml.etree.ElementTree as ET
+
 data = '''<person>
 <name>Dr Chuck</name>
 <phone type="intl">
     +1 734 303 4456
 </phone>
-<email hide="yes"/>
+<email hide="email@email.com"/>
 </person>'''
 
 tree = ET.fromstring(data)
+
 print('Name:', tree.find('name').text)
 print('Attr:', tree.find('email').get('hide'))
